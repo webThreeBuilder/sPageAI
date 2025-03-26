1b95b3bd46a901e97041caefd6a42d0bec322611
webThreeBuilder <44132569+webThreeBuilder@users.noreply.github.com>
2025-03-26 22:57:21 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,20 @@ const nextConfig: NextConfig = {
   },
   output: 'standalone', // 
   poweredByHeader: false, 
+  async headers() {
+    return [
+      {
+        // 为所有API路由添加CORS头
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
